834cc2ada2dbb29cff06dffa76fc20571a8fa0b7
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-03-26 18:32:40 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -2,10 +2,7 @@ class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[2];
         for(int i=0;i<num_list.length;i++){
-            if(num_list[i]%2 == 0)
-                answer[0]++;
-            else if(num_list[i]%2 == 1)
-                answer[1]++;
+            answer[num_list[i]%2]++;
         }
         return answer;
     }
