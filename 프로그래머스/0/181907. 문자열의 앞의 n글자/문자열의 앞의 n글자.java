6513d5f0b2257655ac288a391ef0ae70f8a97904
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-03-28 18:17:33 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.03 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -1,9 +1,7 @@
 class Solution {
     public String solution(String my_string, int n) {
         String answer = "";
-        for(int i=0;i<n;i++){
-            answer += my_string.charAt(i);
-        }
+        answer = my_string.substring(0,n);
         return answer;
     }
 }
