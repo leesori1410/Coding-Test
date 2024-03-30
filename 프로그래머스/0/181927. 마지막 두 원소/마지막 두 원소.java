d26751b5023802cba32c1a7212f4e692efab85e6
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-03-31 00:31:33 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -3,13 +3,14 @@ class Solution {
         // 이 문제에서는 num_list의 마지막 원소와 그전 원소의 값이 같을 때 상황을 생각하지 않고 있다. 
         // 그래서 else가 아닌 else if를 쓰면 "실패"가 뜬다.
         int[] answer = new int[num_list.length+1];
-        for(int i=0;i<num_list.length;i++){
+        for(int i=0; i<num_list.length; i++){
             answer[i] += num_list[i];
         }
         if(num_list[num_list.length-1]>num_list[num_list.length-2])
-            answer[num_list.length] = num_list[num_list.length-1] - num_list[num_list.length-2];
+            answer[answer.length-1] = num_list[num_list.length-1] - num_list[num_list.length-2];
         else
-            answer[num_list.length] = (num_list[num_list.length-1])*2;
+            answer[answer.length-1] = (num_list[num_list.length-1])*2;
+        
         return answer;
     }
 }
