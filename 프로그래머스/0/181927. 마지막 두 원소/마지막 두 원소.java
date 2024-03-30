952d15231a19d33d2f32eb6f7e55af1626b16a97
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-03-31 00:20:17 +0900

    Update 마지막 두 원소(문제 오류).java

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,5 +1,7 @@
 class Solution {
     public int[] solution(int[] num_list) {
+        // 이 문제에서는 num_list의 마지막 원소와 그전 원소의 값이 같을 때 상황을 생각하지 않고 있다. 
+        // 그래서 else가 아닌 else if를 쓰면 "실패"가 뜬다.
         int[] answer = new int[num_list.length+1];
         for(int i=0;i<num_list.length;i++){
             answer[i] += num_list[i];
