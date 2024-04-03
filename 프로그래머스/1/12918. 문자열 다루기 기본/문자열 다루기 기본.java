207207d7fc5d3475bbe66ec7cb023522e3f363d8
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-04-03 11:16:42 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = false;
+        if(s.length() == 4 || s.length() == 6){
+            for(int i=0; i<s.length(); i++){
+             if(s.charAt(i)>=48 && s.charAt(i)<=57)
+                 answer = true;
+             else{
+                 answer = false;
+                 break;
+             }
+           }
+        }
+        return answer;
+    }
+}
