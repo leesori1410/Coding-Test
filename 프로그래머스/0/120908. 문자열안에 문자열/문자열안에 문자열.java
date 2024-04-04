3de61c9b292072d67e8638b9c46ab6ac8a7d67ee
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-04-04 14:58:31 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,18 +1,6 @@
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 2;
-        int count=0;
-        for(int i=0; i<str1.length(); i++){
-            if(str1.charAt(i)==(str2.charAt(count)))
-                count++;
-            else 
-                count=0;
-            if(count==str2.length()){
-                answer = 1;
-                break;
-           }
-        }
-        
+        int answer = (str1.contains(str2) ? 1 : 2);        
         return answer;
     }
 }
