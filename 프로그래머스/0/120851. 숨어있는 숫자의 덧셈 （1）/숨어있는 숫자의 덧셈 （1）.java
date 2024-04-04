fdffe0087ccac0c755b7ad828c11969932c2f5e7
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-04-04 18:36:49 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String my_string) {
         int answer = 0;
         for(int i=0; i<my_string.length(); i++){
-            if(my_string.charAt(i)>=48 && my_string.charAt(i)<=57)
+            if(my_string.charAt(i)>='0' && my_string.charAt(i)<='9')
                 answer += (my_string.charAt(i)-48);
         }
         return answer;
