c86bc8570b44230dbf1e18d7dd38bd96271257ef
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-04-04 11:41:57 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(int n) {
-        int answer = 1;
-        if(Math.sqrt(n)%1 != 0){
-            answer = 2;
-        }
+        int answer = (Math.sqrt(n)%1 == 0) ? 1 : 2;
         return answer;
     }
 }
