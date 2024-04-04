a4832abb929fabf5b5d772038e7ac511ec40758d
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-04-04 14:02:49 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,9 +1,8 @@
 class Solution {
     public int[] solution(int n) {
         int[] answer = new int[(n%2==0) ? n/2 : n/2+1];
-        int index=0;
-        for(int i=1;i<=n; i+=2)
-            answer[index++] = i;
+        for(int i=1, j=0;i<=n; i+=2, j++)
+            answer[j] = i;
         return answer;
     }
 }
