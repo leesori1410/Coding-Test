f18a03e7a28c7beac8c8e1469877ea84e7bab8b3
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-04-08 23:18:34 +0900

    [level 0] Title: rny_string, Time: 0.09 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.java
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -1,14 +1,7 @@
 class Solution {
     public String solution(String rny_string) {
         String answer = "";
-        for(int i=0; i<rny_string.length(); i++){
-            if(rny_string.charAt(i)=='m'){
-                answer+= "rn";
-            }
-            else{
-                answer+= rny_string.charAt(i);
-            }
-        }
+        answer = rny_string.replaceAll("m", "rn");
         return answer;
     }
 }
