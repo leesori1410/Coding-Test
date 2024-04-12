8ee07c97ebc6723ae96318a35d38a4e7f673d20e
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-04-13 00:26:56 +0900

    [level 0] Title: 0 떼기, Time: 7.89 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,18 +1,7 @@
 class Solution {
     public String solution(String n_str) {
         String answer = "";
-        int z=0;
-        for(int i=0; i<n_str.length(); i++){
-            if(z==0){
-                if(n_str.charAt(i)!='0'){
-                    answer += n_str.charAt(i);
-                    z=1;
-                }
-            }
-            else{
-                answer += n_str.charAt(i);
-            }
-        }
+        answer = ""+Integer.parseInt(n_str);
         return answer;
     }
 }
