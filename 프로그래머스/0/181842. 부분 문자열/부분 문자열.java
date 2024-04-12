394fcbf7b3cd23494a171ffa3e6c6834b3ef1e58
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-04-13 00:01:13 +0900

    [level 0] Title: 부분 문자열, Time: 0.03 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
@@ -1,18 +1,20 @@
 class Solution {
     public int solution(String str1, String str2) {
         int answer = 0;
-        for(int i=0; i<str2.length()-str1.length()+1; i++){
-            for(int j=0; j<str1.length(); j++){
-                if(str2.charAt(i+j)==str1.charAt(j))
-                    answer=1;
-                else{
-                    answer = 0;
-                    break;
-                }
-            }
-            if(answer==1)
-                break;
-        }
+        // for(int i=0; i<str2.length()-str1.length()+1; i++){
+        //     for(int j=0; j<str1.length(); j++){
+        //         if(str2.charAt(i+j)==str1.charAt(j))
+        //             answer=1;
+        //         else{
+        //             answer = 0;
+        //             break;
+        //         }
+        //     }
+        //     if(answer==1)
+        //         break;
+        // }
+        if(str2.contains(str1))
+            answer=1;
         return answer;
     }
 }
