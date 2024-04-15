f084fa5fcc9818b3d6201ff021ce85cd777e0adb
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-04-16 00:05:04 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,12 +1,7 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        int answer = 0;
-        String str ="";
-        for(int i=0; i<my_string.length(); i++){
-            str+=my_string.charAt(i);
-            if(str.equals(is_prefix))
-                return 1;
-        }
+        boolean tf=my_string.startsWith(is_prefix);
+        int answer = tf==true ? 1 : 0;
         return answer;
     }
 }
