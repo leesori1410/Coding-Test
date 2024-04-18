6e74be26f093aa963e107f4d500875453bc1a6f0
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-04-18 17:15:37 +0900

    [level 0] Title: 접미사 배열, Time: 0.19 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -4,12 +4,8 @@ class Solution {
     public String[] solution(String my_string) {
         String[] answer = new String[my_string.length()];
         
-        for(int i=my_string.length()-1; i>=0; i--){
-            String my = "";
-            for(int j=my_string.length()-1; j>=i; j--){
-                my=my_string.charAt(j)+my;
-            }
-            answer[i]=my;
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = my_string.substring(i, my_string.length());
         }
         Arrays.sort(answer);
         return answer;
