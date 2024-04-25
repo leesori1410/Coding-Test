2efcea29b15accb5c31ce3c0aa1ec05b7406bbef
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-04-25 20:54:58 +0900

    [level 1] Title: 옹알이 (2), Time: 1.91 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String aya = "aya", ye = "ye", woo = "woo", ma = "ma";
+        for(int i=0; i<babbling.length; i++){
+            int count=0;
+            for(int j=2; j<=15; j++){
+                if(babbling[i].contains(aya.repeat(j)))
+                    babbling[i] = babbling[i].replace(aya.repeat(j), "1");
+                if(babbling[i].contains(ye.repeat(j)))
+                    babbling[i] = babbling[i].replace(ye.repeat(j), "1");
+                if(babbling[i].contains(woo.repeat(j)))
+                    babbling[i] = babbling[i].replace(woo.repeat(j), "1");
+                if(babbling[i].contains(ma.repeat(j)))
+                    babbling[i] = babbling[i].replace(ma.repeat(j), "1");
+            }
+            if(babbling[i].contains(aya))
+                babbling[i] = babbling[i].replace(aya, "0");
+            if(babbling[i].contains(ye))
+                babbling[i] = babbling[i].replace(ye, "0");
+            if(babbling[i].contains(woo))
+                babbling[i] = babbling[i].replace(woo, "0");
+            if(babbling[i].contains(ma))
+                babbling[i] = babbling[i].replace(ma, "0");
+            for(int j=0; j<babbling[i].length(); j++)
+                if(babbling[i].charAt(j)!='0')
+                    count++;
+            if(count==0)
+                answer++;
+        }
+        return answer;
+    }
+}
