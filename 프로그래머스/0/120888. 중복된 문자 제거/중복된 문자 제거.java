e4ca264053d2587e55dddbf3ce38e4c6115bad14
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-05-02 14:06:25 +0900

    [level 0] Title: 중복된 문자 제거, Time: 9.65 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        char[] ch = my_string.toCharArray();
+        for(int i=0; i<ch.length; i++){
+            for(int j=i+1; j<ch.length; j++){
+                if(ch[i]==ch[j])
+                    ch[j]='0';
+            }
+        }
+        for(int i=0; i<ch.length; i++){
+            if(ch[i]!='0')
+                answer += ch[i];
+        }
+        return answer;
+    }
+}
