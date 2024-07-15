9ede9ea6956a180034a15b59efc7d950e0e4b7c0
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-07-15 23:20:00 +0900

    [level 1] Title: 폰켓몬, Time: 0.80 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,8 +1,2 @@
 def solution(nums):
-    answer = 0
-    n = len(set(nums))
-    if n > len(nums)/2:
-        answer = len(nums)/2
-    else:
-        answer = n
-    return answer
+    return min(len(nums)/2, len(set(nums)))
