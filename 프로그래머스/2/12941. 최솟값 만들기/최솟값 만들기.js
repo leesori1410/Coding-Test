8a0b3985e25b2c3556a6544d3c504d809f3ed4f0
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-08-26 21:17:26 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.80 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,8 +1,7 @@
 function solution(A,B){
     var answer = 0;
     A.sort((a, b) => a - b);
-    B.sort((a, b) => a - b);
-    B.reverse();
+    B.sort((a, b) => b - a);
     for(let i in A)
         answer += A[i] * B[i];
     
