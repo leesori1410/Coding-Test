df846eee3f33f85a220395c6389694da25271128
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2024-09-02 19:24:15 +0900

    [level 2] Title: 짝지어 제거하기, Time: 50.60 ms, Memory: 43.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,16 @@
+function solution(s)
+{
+    let st = [];
+    let answer = 0;
+    for(let i = 0; i < s.length; i++){
+        if(st.length === 0)
+            st.push(s[i]);
+        else if(st[st.length-1] != s[i])
+            st.push(s[i]);
+        else
+            st.pop();
+    }
+    if(st.length === 0)
+        answer = 1;
+    return answer;
+}
