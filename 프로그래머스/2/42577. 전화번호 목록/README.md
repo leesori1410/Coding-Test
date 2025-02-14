72b2d737328927f7a60de65edb255eb97c5157d5
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2025-02-15 02:06:08 +0900

    [level 2] Title: 전화번호 목록, Time: 344.57 ms, Memory: 99.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 101 MB, 시간: 259.21 ms
+메모리: 99.5 MB, 시간: 344.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 15일 01:56:35
+2025년 02월 15일 02:06:09
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,28 +3,35 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        Map<Character, List<String>> phoneM = new HashMap<>();
-        for(char c='0'; c<='9'; c++){
-            phoneM.put(c, new ArrayList<>());
+//         Map<Character, List<String>> phoneM = new HashMap<>();
+//         for(char c='0'; c<='9'; c++){
+//             phoneM.put(c, new ArrayList<>());
+//         }
+        
+//         for(String phone : phone_book){
+//             phoneM.get(phone.charAt(0)).add(phone);
+//         }
+//         for(char c='0'; c<='9'; c++){
+//             List<String> phoneL = phoneM.get(c);
+//             if (phoneL != null) Collections.sort(phoneL);
+//         }
+        
+//         for(char c='0'; c<='9'; c++){
+//             List<String> phoneL = phoneM.get(c);
+//             if (phoneL == null) continue;
+//             for(int j=0; j<phoneL.size()-1; j++) {
+//                 if (phoneL.get(j+1).startsWith(phoneL.get(j))) {
+//                     return false;
+//                 }
+//             }
+//         }
+        
+        Arrays.sort(phone_book);
+        for(int i=0; i<phone_book.length-1; i++){
+            if(phone_book[i+1].startsWith(phone_book[i]))
+                return false;
         }
         
-        for(String phone : phone_book){
-            phoneM.get(phone.charAt(0)).add(phone);
-        }
-        for(char c='0'; c<='9'; c++){
-            List<String> phoneL = phoneM.get(c);
-            if (phoneL != null) Collections.sort(phoneL);
-        }
-        
-        for(char c='0'; c<='9'; c++){
-            List<String> phoneL = phoneM.get(c);
-            if (phoneL == null) continue;
-            for(int j=0; j<phoneL.size()-1; j++) {
-                if (phoneL.get(j+1).startsWith(phoneL.get(j))) {
-                    return false;
-                }
-            }
-        }
         
         return answer;
     }
