ab8b4c650d6e4fa6755c498f67e3a5712258e9f4
leesori1410 <128341261+leesori1410@users.noreply.github.com>
2025-02-15 01:56:35 +0900

    [level 2] Title: 전화번호 목록, Time: 259.21 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Map<Character, List<String>> phoneM = new HashMap<>();
+        for(char c='0'; c<='9'; c++){
+            phoneM.put(c, new ArrayList<>());
+        }
+        
+        for(String phone : phone_book){
+            phoneM.get(phone.charAt(0)).add(phone);
+        }
+        for(char c='0'; c<='9'; c++){
+            List<String> phoneL = phoneM.get(c);
+            if (phoneL != null) Collections.sort(phoneL);
+        }
+        
+        for(char c='0'; c<='9'; c++){
+            List<String> phoneL = phoneM.get(c);
+            if (phoneL == null) continue;
+            for(int j=0; j<phoneL.size()-1; j++) {
+                if (phoneL.get(j+1).startsWith(phoneL.get(j))) {
+                    return false;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
